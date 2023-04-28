23e805c03c5b3b7d87401e53ac4fb8c15ff80fa4
Samy4-byte <samatbayizov@gmail.com>
2023-04-28 17:19:13 +0600

    add test style

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -2,22 +2,23 @@ import { Error } from "../../components/error"
 import { Loader } from "../../components/loader"
 import { Question } from "../../components/question"
 import { useTestPage } from "./useTestsPage"
-
+import NavStyle  from "./test.module.css"
 
 export const TestPage = () => {
   const { tests, loading, error } = useTestPage()
 
   return (
-    <div >
-      <h1>Вопросы</h1>
-        {loading && <Loader />}
+    <div className={NavStyle.content}>
+      {loading && <Loader />}
+      <h1>Вопрос №</h1>
+      <div className={NavStyle.bigContainer}>
         {error && <Error />}
-      {
-        tests.map((e) => (
-          <Question key={e.id} data={e} />
-        ))
-      }
-      TestPage
+        {
+          tests.map((e) => (
+            <Question key={e.id} data={e} />
+          ))
+        }
+      </div>
     </div>
   )
 }
