88dd82b1c1be65dbdfa482075c8fee293f8c62c6
Samy4-byte <samatbayizov@gmail.com>
2023-04-28 15:38:56 +0600

    add style color

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../header/Header.jsx'
 import Footer from '../footer/Footer.jsx'
+import NavStyle from './layout.module.css'
+
 
 export const Layout = () => {
   return (
-    <div>
+    <div className={NavStyle.container}>
       <Header />
 
       <div>
         <Outlet />
       </div>
+      
       <Footer />
     </div>
   )
