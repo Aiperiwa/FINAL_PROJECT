117a76ad600efcb8165e80624f40eb12094110db
Samy4-byte <samatbayizov@gmail.com>
2023-05-05 11:53:52 +0600

    add burger adapt

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,33 +1,4 @@
-// import { Error } from "../../components/error"
-// import { Loader } from "../../components/loader"
-// import { Question } from "../../components/question"
-// import { useTestPage } from "./useTestsPage"
-// import NavStyle from "./test.module.css"
-// import { CountDown} from '../../components/timer/timer.jsx'
 
-
-// export const TestPage = () => {
-//   const { loading, tests, error } = useTestPage()
-
-//   return (
-//     <div className={NavStyle.content}>
-//       {loading ? <Loader /> : null}
-//       {loading ? null : (
-//         <div className={NavStyle.bigContainer}>
-//           <h1>Онлайн-тест ПДД КР 2023</h1>
-
-//             <div>{ CountDown } </div>
-//           {/* {error && <Error />} */}
-//           {
-//             tests.map((e) => (
-//               <Question key={e.id} data={e} />
-//             ))
-//           }
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
 
 import React from "react";
 import CountDown from '../../components/timer/timer.jsx';
@@ -49,11 +20,13 @@ export const TestPage = () => {
           <CountDown minutes={20} seconds={0} isOver={false} resultInfo={{ errors: 0 }} />
           {error && <Error />}
           {
-               tests.map((e) => (
+            tests.map((e) => (
               <Question key={e.id} data={e} />
             ))
           }
-          <button className={NavStyle.check} >Проверить</button>
+          <div className={NavStyle.btnContainer}>
+            <button className={NavStyle.check} >Проверить</button>
+          </div>
         </div>
       )}
     </div>
